642295fcd5d9446b2c05557567beaa9845173ada
Shota Tamura <junkboy0315@gmail.com>
2018-03-26 11:18:35 +0900

    remove default css

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
-import './index.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -15,16 +14,8 @@ const TemplateWrapper = ({ children }) => (
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+
+    {children()}
   </div>
 )
 
