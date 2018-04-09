20ccb4d38111d940e862bfbabfabdf8085e1fef7
Shota Tamura <junkboy0315@gmail.com>
2018-04-09 16:57:47 +0900

    add profile footer

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -90,6 +90,47 @@ export default ({data}) => {
       borderBottom: '1px dotted rgba(0,0,0,0.5)',
       marginBottom: '2rem',
       textAlign: 'left',
+    },
+    footer: {
+      display: 'flex',
+      marginTop: '2rem',
+      padding: '2rem',
+
+      '@media (max-width: 499px)': {
+        flexDirection: 'column',
+        ' img': {
+          width: '100px',
+          margin: '0 auto 2rem',
+        },
+        ' div p:first-child': {
+          fontWeight: 'bold',
+          marginBottom: '1rem',
+          textAlign: 'center',
+        },
+        ' div p:last-child': {
+          fontSize: '0.8rem',
+        },
+        ' p': {
+          marginBottom: 0,
+        },
+      },
+      '@media (min-width: 500px)': {
+        display: 'flex',
+        ' img': {
+          height: '100px',
+          marginRight: '2rem',
+        },
+        ' div p:first-child': {
+          fontWeight: 'bold',
+          marginBottom: '1rem',
+        },
+        ' div p:last-child': {
+          fontSize: '0.8rem',
+        },
+        ' p': {
+          marginBottom: 0,
+        }
+      }
     }
   };
 
@@ -99,23 +140,36 @@ export default ({data}) => {
     <SectionContainer colorNumber={1} isTop={true}>
       <SectionHeader colorNumber={1} text={'YUUNIWORKS ブログ'} />
 
-      <article css={styles.container}>
+      <div css={styles.container}>
       
       <Link to="/blog" css={styles.backButton}><span className="fas fa-arrow-left" /> 記事一覧に戻る</Link>
 
       <WhiteBox>
-        <div css={styles.boxInner}>
+        <article css={styles.boxInner}>
           <time css={styles.datetime} dateTime={post.frontmatter.date}>
             {post.frontmatter.date.slice(0,10)}
           </time>
           <h1 css={styles.title}>{post.frontmatter.title}</h1>
           <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        </div>
+        </article>
       </WhiteBox>
-      
+
+      <WhiteBox>
+        <footer css={styles.footer}>
+          <img src="/images/profile.png" />
+          <div>
+            <p>田村 翔太</p>
+            <p>YUUNIWORKS代表。島根県浜田市を拠点に主にフロントエンド開発のお手伝いをしているフリーランスエンジニアです。React/Reduxを用いたSingle Page Applicationや、NodeJSを使ったAPI開発を得意としています。</p>
+          </div>
+        </footer>
+      </WhiteBox>
+
+    
       <Link to="/blog" css={styles.backButton}><span className="fas fa-arrow-left" /> 記事一覧に戻る</Link>
       
-      </article>
+      </div>
+
+
 
     </SectionContainer>
   )
