853d6797a982db12b0fa5835315f6f78aeacfb38
Shota Tamura <junkboy0315@gmail.com>
2018-04-10 11:57:52 +0900

    solve a CORS related error

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -25,7 +25,7 @@ const TemplateWrapper = ({ children, location }) => {
           {
             src: 'https://use.fontawesome.com/releases/v5.0.8/js/all.js',
             integrity:'sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ',
-            crossorigin: 'anonymous',
+            crossOrigin: 'anonymous',
           },
         ]}
       >
