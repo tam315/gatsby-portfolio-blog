59d2ba2917a3a7b1097d8eaabfcf1906af2aa00b
stam <junkboy0315@gmail.com>
2018-04-13 13:53:28 +0900

    add a lambda function

---
+++ function/send-email/functions/submit/index.js
@@ -0,0 +1,13 @@
+const sendEmail = require('./send');
+console.log('starting function');
+
+exports.handle = function(e, ctx, cb) {
+  console.log('processing event: %j', e);
+  if (e.subject && e.body) {
+    sendEmail(e.subject, e.body).then((result) => {
+      cb(null, result);
+    }, cb);
+  } else {
+    cb(new Error('Invalid payload: ' + JSON.stringify(e)));
+  }
+};
