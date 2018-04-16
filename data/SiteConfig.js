84148ec78f072ece2a9cb001dca326557f86ebe5
stam <junkboy0315@gmail.com>
2018-04-16 10:07:56 +0900

    add config file

---
+++ data/SiteConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteTitle: 'Yuuniworks / 島根のフリーランスエンジニア',
+  siteDescription: '島根県浜田市を拠点に、主にフロントエンド開発のお手伝いをしているフリーランスエンジニアです。',
+  siteKeywords: 'SPA, Faas, Serverless, React, Angular, フリーランス',
+  siteUrl: `https://www.yuuniworks.com`,
+  siteLogo: '/images/logo_for_schema.png',
+  googleAnalyticsID: 'UA-116967778-1',
+}
