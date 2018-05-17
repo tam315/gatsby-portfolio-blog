62756dfa9a287919344f186f27dbda5c74b0df2d
stam <junkboy0315@gmail.com>
2018-05-17 13:22:38 +0900

    prettier

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,8 +1,10 @@
 module.exports = {
   corporationName: 'Yuuniworks',
   siteTitle: 'Yuuniworks / 島根のフリーランスエンジニア',
-  siteDescription: 'フリーランスのフロントエンドエンジニアです。React/Reduxを用いたSPAや、NodeJSによるAPI開発を得意としています。',
-  siteKeywords: 'SPA,Serverless,サーバーレス,Lambda,AWS,React,Redux,Angular,フリーランス',
+  siteDescription:
+    'フリーランスのフロントエンドエンジニアです。React/Reduxを用いたSPAや、NodeJSによるAPI開発を得意としています。',
+  siteKeywords:
+    'SPA,Serverless,サーバーレス,Lambda,AWS,React,Redux,Angular,フリーランス',
   siteUrl: 'https://www.yuuniworks.com',
   siteLogo: '/images/logo_for_schema.png',
   googleAnalyticsID: 'UA-116967778-1',
