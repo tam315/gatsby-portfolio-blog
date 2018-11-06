a45caf46f2e7addd8c8e3c3251fc73a4800c66b2
stam <junkboy0315@gmail.com>
2018-11-06 14:28:08 +0900

    always invoke adsbygoogle.push()

--- src/components/GoogleAds/index.jsx
+++ src/components/GoogleAds/index.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
 import Helmet from 'react-helmet';
+import React from 'react';
 
 class GoogleAds extends React.Component {
-  componentWillMount = () => {
+  componentDidMount = () => {
     // quit if SSR
     if (typeof window === 'undefined') return;
 
     window.adsbygoogle = window.adsbygoogle || [];
 
-    // if already initialized
-    if (!window.adsbygoogle.length) return;
-
-    window.adsbygoogle.push({
-      google_ad_client: 'ca-pub-7134126650568891',
-      enable_page_level_ads: true,
-    });
+    try {
+      window.adsbygoogle.push({
+        google_ad_client: 'ca-pub-7134126650568891',
+        enable_page_level_ads: true,
+      });
+    } catch (err) {
+      console.log('TODO: fix error', err);
+    }
   };
 
   render = () => (
-    <div>
-      <Helmet>
-        <script
-          async
-          src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        />
-        <script />
-      </Helmet>
-    </div>
+    <Helmet>
+      <script
+        async
+        src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+      <script />
+    </Helmet>
   );
 }
 
